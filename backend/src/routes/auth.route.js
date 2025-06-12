7299e047a9ddd38a60e7ae3fa1af12e7556d9690
jamyang dolma <jamyangdolma30@gmil.com>
2025-06-12 10:25:19 +0600

    added auth routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,1 +1,10 @@
-//auth route
+const verifyAuth = require("../middlewares/verifyAuth.middleware");
+const authController = require("../controllers/auth.controller");
+const authRoutes = express.Router();
+
+authRoutes.post("/signin", authController.signIn);
+
+authRoutes.post("/signup", authController.signUp);
+authRoutes.delete("/signout", verifyAuth, authController.signout);
+
+module.exports = authRoutes;

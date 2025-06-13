920b69943dd306269b6818a07bc1f5912162f242
Pema Wangchuk <pewangk703@gmail.com>
2025-06-13 09:48:46 -0400

    added CORS middleware to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const cors = require("cors");
 const connectMongoDB = require("./db/mongo.db");
 const authRoutes = require("./routes/auth.route");
 const moviesRoutes = require("./routes/movie.route");
@@ -10,6 +10,12 @@ connectMongoDB();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/movies", moviesRoutes);
 app.use("/auth", authRoutes);

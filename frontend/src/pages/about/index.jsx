96ba75c2635cf7d834ec3335a5f9cf37cf91ecf2
Phurba wangmo <phurbasekai134@gmail.com>
2025-06-17 21:42:22 -0700

    added team

---
+++ frontend/src/pages/about/index.jsx
@@ -0,0 +1,15 @@
+import Team from "./Team";
+
+const About = () => {
+  return (
+    <div>
+      <h1>
+        Intro to our team of passionate learners who joined forces to create a
+        fun, dynamic movie website that showcases our creativity, teamwork, and
+        love for web development.
+      </h1>
+      <Team />
+    </div>
+  );
+};
+export default About;

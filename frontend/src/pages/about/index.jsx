32972f9e49b7e212d333eee2be1d386e768ff161
Phurba wangmo <phurbasekai134@gmail.com>
2025-06-18 22:21:09 -0700

    made necessary changes in team file

--- frontend/src/pages/about/index.jsx
+++ frontend/src/pages/about/index.jsx
@@ -2,14 +2,15 @@ import Team from "./Team";
 
 const About = () => {
   return (
-    <div>
-      <h1>
+    <div style={{ textAlign: "center", color: "darkblue", padding: "20px" }}>
+      <h2>
         Intro to our team of passionate learners who joined forces to create a
         fun, dynamic movie website that showcases our creativity, teamwork, and
         love for web development.
-      </h1>
+      </h2>
       <Team />
     </div>
   );
 };
+
 export default About;
